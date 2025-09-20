311039df531c2cac89cd28d89406e94a36cd2115
Juan Carlos Sánchez Martínez <johncard962@gmail.com>
2025-09-20 15:07:11 -0600

    apllied new styles

--- js/helpers.js
+++ js/helpers.js
@@ -55,18 +55,25 @@ function acortarTexto(texto, maxLongitud) {
   return texto.slice(0, maxLongitud) + '...';
 }
 
-function SearchForm(searchForm, cards_container){
-    searchForm.addEventListener('submit', function(event) {
-        event.preventDefault();
-        cards_container.innerHTML = '';
-        let searchInput = document.getElementById('search_input').value.toLowerCase();
-        let listData = mainData(`gallery?price=${searchInput}`);
-        listData.then(data => {
-            data.data.forEach(item => {
-                cards_container.innerHTML += cardTemplate(item.name, item.description, item.category.name, item.price);
-            })
-        })
-    })
+function cardTemplate(title, paragraph, category, price, img) {
+    return `
+    <div class="col-xxl-4">
+        <div class="card">
+            <div class="row align-items-center min-h-300 px-3">
+                <div class="col-md-4">
+                    <img src="${img.replace('http://127.0.0.1:8000/https%3A', '/')}" class="img-fluid rounded" alt="Desktop MacBook">
+                </div>
+                <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">${title}</h5>
+                        <p class="card-text">${acortarTexto(paragraph, 250)}</p>
+                        <p class="card-text">Category - ${category}</p>
+                        <p class="card-text">Price - <mark>$${price}</mark></p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>`;
 }
 
-export { mainData, getCookie, acortarTexto, SearchForm };
+export { mainData, getCookie, acortarTexto, cardTemplate };
--- js/script.js
+++ js/script.js
@@ -1,26 +1,6 @@
-import { mainData, acortarTexto, SearchForm } from "./helpers.js";
+import { mainData, cardTemplate } from "./helpers.js";
 
 // index.html
-function cardTemplate(title, paragraph, category, price, img) {
-    return `
-    <div class="col-xxl-4">
-        <div class="card">
-            <div class="row align-items-center">
-                <div class="col-md-4">
-                    <img src="${img.replace('http://127.0.0.1:8000/https%3A', '/')}" class="img-fluid rounded" alt="Desktop MacBook">
-                </div>
-                <div class="col-md-8">
-                    <div class="card-body">
-                        <h5 class="card-title">${title}</h5>
-                        <p class="card-text">${acortarTexto(paragraph, 250)}</p>
-                        <p class="card-text">Category - ${category}</p>
-                        <p class="card-text">Price - <mark>$${price}</mark></p>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>`;
-}
 
 const cards_container = document.getElementById('cards_container');
 
@@ -33,4 +13,29 @@ itemsList.then(data => {
 .catch(error => cards_container.innerHTML = `<h2 class="text-center fw-bold">No items found: ${error}</h2>`);
 
 let searchForm = document.getElementById('search_form');
-SearchForm(searchForm, cards_container);
+searchForm.addEventListener('submit', function(event) {
+        event.preventDefault();
+        cards_container.innerHTML = '';
+        let searchInput = document.getElementById('search_input').value.toLowerCase();
+        let listData = mainData(`gallery?category__name=${searchInput}`);
+        listData.then(data => {
+            data.data.forEach(item => {
+                cards_container.innerHTML += cardTemplate(item.name, item.description, item.category.name, item.price, item.image);
+            })
+        })
+})
+
+// Excel report generation
+document.getElementById('excelReport').addEventListener('click', async () => {
+    const response = await fetch('http://127.0.0.1:8000/ecommerce/ecommerce-report');
+    const blob = await response.blob();
+    const url = window.URL.createObjectURL(blob);
+
+    const a = document.createElement('a');
+    a.href = url;
+    a.download = 'excel_report.xlsx';
+    document.body.appendChild(a);
+    a.click();
+    a.remove();
+    window.URL.revokeObjectURL(url);
+});
